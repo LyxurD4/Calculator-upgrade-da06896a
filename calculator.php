490b4ca6815087780048429ec09b2dd9c9a8eb6e
MAURITS DOES <2111633@talnet.nl>
2019-10-02 10:44:26 +0200

    Uitwerking van de exercise

--- calculator.php
+++ calculator.php
@@ -1,6 +1,8 @@
 <?php
-echo "Rekenen zuigt, maar daar heb je mij voor neef. Pass me je operatie(+ , -)\n";
+echo "Rekenen zuigt, maar daar heb je mij voor neef. Pass me je operatie(+ , -, %)\n";
 $a = readline();
+if ($a == "*")
+    echo "Ik kan geen vermedigvuldigingen", exit;
 echo "Sterk neef. Wats je eerste cijfer\n";
 $b = readline();
 
@@ -8,19 +10,22 @@ $b = readline();
 
         echo "Je tweede getal?\n";
         $c = readline();
-            if (is_numeric($c)) {   
+            if (is_numeric($c)) {
 
                 if ($a == "+") 
                 echo "Je antwoord is : ". ($b + $c);
 
                 if ($a == "-")
                 echo "Je antwoord is : ". ($b - $c);
+
+                if ($a == "%")
+                echo "Je antwoord is : ". ($b % $c);
                 
         } else {
-            echo var_export($c, true) . " is NOT numeric", exit();
-        } 
+            echo var_export($c, true) . " is NOT numeric", exit("\nFek of grappenmaker");
+        }
 
     } else {
-        echo var_export($b, true) . " is NOT numeric", exit();
+        echo var_export($b, true) . " is NOT numeric", exit("\nGeen numeriek symbool");
     }
 ?>
